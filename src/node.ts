615c6c35834233aef9abe8f08aa26fe2fb7ce198
Xie Yuheng <xyheme@gmail.com>
2022-02-14 00:02:51 +0800

    up

--- src/node.ts
+++ src/node.ts
@@ -40,20 +40,4 @@ export class Node {
   get arity(): number {
     return this.types.length - 1
   }
-
-  // getType(index: number): Type {
-  //   const t = this.types[index]
-  //   if (t === undefined) {
-  //     throw new Error(
-  //       [
-  //         `Can not get type of node`,
-  //         `  index: ${index}`,
-  //         `  id: ${this.id}`,
-  //         `  name: ${this.name}`,
-  //       ].join("\n")
-  //     )
-  //   }
-
-  //   return t
-  // }
 }
