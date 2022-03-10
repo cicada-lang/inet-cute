18d684e288812951d500a4e7ae142202d637c4cb
Xie Yuheng <xyheme@gmail.com>
2022-03-10 10:22:51 +0800

    up

---
+++ docs/articles/shallo-embedding.ts
@@ -0,0 +1,17 @@
+Module.create()
+  .node(`sole`, { output: `Trivial *` })
+  .node(`null`, { output: `TrivialList *` })
+  .node(`cons`, { input: `TrivialList Trivial`, output: `Nat *` })
+  .node(`append`, { input: `TrivialList TrivialList *`, output: `TrivialList` })
+
+  .rule(`null append`, ``)
+  .rule(`cons append`, `rot rot append swap cons`)
+
+  .net(`six-soles`, {
+    output: `TrivialList`,
+    body: `
+     null sole cons sole cons sole cons
+     null sole cons sole cons sole cons
+     append
+   `,
+  })
