50c78463c0d1c1f86aeec3a2d1ae8840bbf785e1
Xie Yuheng <xyheme@gmail.com>
2023-07-28 17:09:07 +0800

    `NetRenderer` -- format to simple text

--- src/renderers/NetRenderer.ts
+++ src/renderers/NetRenderer.ts
@@ -14,30 +14,28 @@ export class NetRenderer {
       const start = formatNode(edge.start.node)
       const end = formatNode(edge.end.node)
       const indexes = `${edge.end.index}-${edge.start.index}`
-      lines.push(`"${end}" -- "${start}" [label="${indexes}"];`)
-      lines.push(`"${start}";`)
-      lines.push(`"${end}";`)
+      lines.push(`${end} -- ${start} [label="${indexes}"]`)
+      lines.push(`${start}`)
+      lines.push(`${end}`)
     }
 
     for (const edge of net.actions) {
       const start = formatNode(edge.start.node)
       const end = formatNode(edge.end.node)
       const indexes = `${edge.end.index}-${edge.start.index}`
-      lines.push(`"${end}" -- "${start}" [label="${indexes}" color=red];`)
-      lines.push(`"${start}";`)
-      lines.push(`"${end}";`)
+      lines.push(`${end} -- ${start} [label="${indexes}" color=red]`)
+      lines.push(`${start}`)
+      lines.push(`${end}`)
     }
 
     for (const port of net.portStack) {
       const start = formatNode(port.node)
       const end = `${formatNode(port.node)}(${port.index})`
-      lines.push(`"${end}" -- "${start}";`)
-      lines.push(`"${start}";`)
-      lines.push(`"${end}";`)
+      lines.push(`${end} -- ${start}`)
+      lines.push(`${start}`)
+      lines.push(`${end}`)
     }
 
-    const body = lines.join(" ")
-
-    return `graph { ${body} }`
+    return lines.join("\n")
   }
 }
