dc51c7fe2705bbdb39b67c28340901d1da037d49
Xie Yuheng <xyheme@gmail.com>
2023-07-30 02:02:45 +0800

    `NetRenderer` improve format

--- src/renderers/NetRenderer.ts
+++ src/renderers/NetRenderer.ts
@@ -13,21 +13,17 @@ export class NetRenderer {
     for (const edge of net.activeEdges) {
       const start = formatNode(edge.start.node)
       const end = formatNode(edge.end.node)
-      lines.push(
-        `(${start})-[${edge.start.index}]![${edge.end.index}]-(${end})`,
-      )
+      lines.push(`(${start})-${edge.start.index}!${edge.end.index}-(${end})`)
     }
 
     for (const edge of net.edges) {
       const start = formatNode(edge.start.node)
       const end = formatNode(edge.end.node)
-      lines.push(
-        `(${start})-[${edge.start.index}] [${edge.end.index}]-(${end})`,
-      )
+      lines.push(`(${start})-${edge.start.index} ${edge.end.index}-(${end})`)
     }
 
     for (const port of net.portStack) {
-      lines.push(`(${formatNode(port.node)})-[${port.index}]`)
+      lines.push(`(${formatNode(port.node)})-${port.index}`)
     }
 
     return lines.join("\n")
