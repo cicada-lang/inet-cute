14d7da728571152408509cc27fbfef1515d7642a
Xie Yuheng <xyheme@gmail.com>
2023-07-30 17:33:16 +0800

    inline `createEdge` and `createActiveEdge` to `connect`

--- src/lang/graph/connect.ts
+++ src/lang/graph/connect.ts
@@ -1,13 +1,18 @@
-import { Net, Port, createEdge } from "."
+import { Net, Port } from "."
 import { lookupRuleByPorts } from "../mod/lookupRuleByPorts"
-import { createActiveEdge } from "./createActiveEdge"
 
 export function connect(net: Net, start: Port, end: Port): void {
   const rule = lookupRuleByPorts(net.mod, start, end)
 
   if (rule !== undefined) {
-    net.activeEdges.push(createActiveEdge(start, end, rule))
+    const edge = { start, end, rule }
+    start.connection = { edge, port: end }
+    end.connection = { edge, port: start }
+    net.activeEdges.push(edge)
   } else {
-    net.edges.push(createEdge(start, end))
+    const edge = { start, end }
+    start.connection = { edge, port: end }
+    end.connection = { edge, port: start }
+    net.edges.push(edge)
   }
 }
