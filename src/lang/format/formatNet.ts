fd137123a9f74bb50ca824928da1e2d29d175e9c
Xie Yuheng <xyheme@gmail.com>
2023-07-30 22:49:53 +0800

    `formatNet` -- reverse edge order

--- src/lang/format/formatNet.ts
+++ src/lang/format/formatNet.ts
@@ -4,13 +4,13 @@ import { formatNode } from "./formatNode"
 export function formatNet(net: Net): string {
   const lines: Array<string> = []
 
-  for (const edge of net.activeEdges) {
+  for (const edge of [...net.activeEdges].reverse()) {
     const start = formatNode(edge.start.node)
     const end = formatNode(edge.end.node)
     lines.push(`(${start})-${edge.start.name}!${edge.end.name}-(${end})`)
   }
 
-  for (const edge of net.edges) {
+  for (const edge of [...net.edges].reverse()) {
     const start = formatNode(edge.start.node)
     const end = formatNode(edge.end.node)
     lines.push(`(${start})-${edge.start.name} ${edge.end.name}-(${end})`)
