67de805cfd7640e9f568bcafcf1e4960286b1e86
Xie Yuheng <xyheme@gmail.com>
2023-07-30 17:50:46 +0800

    up

--- src/lang/mod/createMod.ts
+++ src/lang/mod/createMod.ts
@@ -1,9 +1,8 @@
-import { Definition } from "../definition"
 import { Mod } from "./Mod"
 import { defineBuiltInOperators } from "./defineBuiltInOperators"
 
 export function createMod(url: URL): Mod {
-  const definitions: Map<string, Definition> = new Map()
+  const definitions = new Map()
   const rules = new Map()
 
   const mod = { url, definitions, rules }
