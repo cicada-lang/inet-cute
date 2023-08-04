baf115d9e5d23e7636c8f3d502d4a0891260a391
Xie Yuheng <xyheme@gmail.com>
2023-08-04 22:16:06 +0800

    up

--- src/lang/cut/cut.ts
+++ src/lang/cut/cut.ts
@@ -15,59 +15,66 @@ export interface CutOptions {
 }
 
 export function cut(mod: Mod, ctx: Ctx, word: Word, options: CutOptions): void {
-  switch (word.kind) {
-    case "Call": {
-      const found = ctx.localSignedTypes.get(word.name)
-      if (found !== undefined) {
-        ctx.signedTypes.push(found)
-        ctx.localSignedTypes.delete(word.name)
-        return
-      } else {
-        const definition = lookupDefinitionOrFail(mod, word.name)
-        cutDefinition(ctx, definition, options)
+  try {
+    switch (word.kind) {
+      case "Call": {
+        const found = ctx.localSignedTypes.get(word.name)
+        if (found !== undefined) {
+          ctx.signedTypes.push(found)
+          ctx.localSignedTypes.delete(word.name)
+          return
+        } else {
+          const definition = lookupDefinitionOrFail(mod, word.name)
+          cutDefinition(ctx, definition, options)
+          return
+        }
+      }
+
+      case "LocalSet": {
+        const signedType = ctx.signedTypes.pop()
+
+        if (signedType === undefined) {
+          throw new Error(
+            `[cut / LocalSet] expect a signed type on the top of the stack`,
+          )
+        }
+
+        ctx.localSignedTypes.set(word.name, signedType)
         return
       }
-    }
 
-    case "LocalSet": {
-      const signedType = ctx.signedTypes.pop()
-
-      if (signedType === undefined) {
-        throw new Error(
-          `[cut / LocalSet] expect a signed type on the top of the stack`,
+      case "PortPush": {
+        const currentSignedType = findCurrentSignedTypeOrFail(
+          word.nodeName,
+          word.portName,
+          options,
         )
+
+        ctx.signedTypes.push(currentSignedType)
+        return
       }
 
-      ctx.localSignedTypes.set(word.name, signedType)
-      return
-    }
+      case "PortReconnect": {
+        const currentSignedType = findCurrentSignedTypeOrFail(
+          word.nodeName,
+          word.portName,
+          options,
+        )
 
-    case "PortPush": {
-      const currentSignedType = findCurrentSignedTypeOrFail(
-        word.nodeName,
-        word.portName,
-        options,
-      )
+        const topSignedType = ctx.signedTypes.pop()
+        if (topSignedType === undefined) {
+          console.error({ currentSignedType })
+          throw new Error(`[cut / PortReconnect] expect top port`)
+        }
 
-      ctx.signedTypes.push(currentSignedType)
-      return
-    }
-
-    case "PortReconnect": {
-      const currentSignedType = findCurrentSignedTypeOrFail(
-        word.nodeName,
-        word.portName,
-        options,
-      )
-
-      const topSignedType = ctx.signedTypes.pop()
-      if (topSignedType === undefined) {
-        console.error({ currentSignedType })
-        throw new Error(`[cut / PortReconnect] expect top port`)
+        unifySignedTypes(ctx, currentSignedType, topSignedType)
+        return
       }
-
-      unifySignedTypes(ctx, currentSignedType, topSignedType)
-      return
     }
+  } catch (error) {
+    if (!(error instanceof Error)) throw error
+
+    console.error({ word })
+    throw new Error(`[cut] Fail to cut word\n` + error.message)
   }
 }
