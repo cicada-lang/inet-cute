b65c2ecef6954e4afe66460c5e33faac2ed660f8
Xie Yuheng <xyheme@gmail.com>
2023-08-06 02:20:48 +0800

    note about createReport

--- src/lang/errors/createReport.ts
+++ src/lang/errors/createReport.ts
@@ -1,6 +1,9 @@
 import { Report, ReportEntry } from "./Report"
 
 export function createReport(error: unknown, entry: ReportEntry): Report {
+  // NOTE We put the most recent report entry at the end,
+  // because the end is closer to user's terminal output.
+
   if (error instanceof Report) {
     error.entries.push(entry)
     return error
