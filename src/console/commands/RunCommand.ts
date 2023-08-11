a4e76a4158b40379febf4acf063ae3d8dd1ddab0
Xie Yuheng <xyheme@gmail.com>
2023-08-11 19:14:14 +0800

    fix url format for windows

--- src/console/commands/RunCommand.ts
+++ src/console/commands/RunCommand.ts
@@ -49,7 +49,14 @@ export class RunCommand extends Command<Args, Opts> {
       },
 
       formatURL(url) {
-        return relative(process.cwd(), url.pathname)
+        if (process.platform === "win32") {
+          return relative(process.cwd(), url.pathname.slice(1)).replaceAll(
+            "\\",
+            "/",
+          )
+        } else {
+          return relative(process.cwd(), url.pathname)
+        }
       },
     })
 
