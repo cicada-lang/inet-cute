b1d6df74de9d63fc6f5d2b5e44c704f000885bf1
Xie Yuheng <xyheme@gmail.com>
2023-08-16 18:33:41 +0800

    comment about `Net`

--- src/lang/net/Net.ts
+++ src/lang/net/Net.ts
@@ -3,10 +3,30 @@ import { Port } from "../port"
 import { Sign } from "../sign"
 import { Value } from "../value"
 
-export type Connection = {
-  edge: Edge
-  port: Port
+/*
+
+  A net is an undirected graph where each edge
+  must be connected through ports.
+
+  We implement a net like adjacency list,
+  but the list of nodes is replaced by a record of ports.
+
+*/
+
+export type Net = {
+  edges: Array<Edge>
+  activeEdges: Array<Edge>
+  nodeEntries: Map<string, NodeEntry>
 }
+
+export type NodeEntry = {
+  id: string
+  name: string
+  url: URL
+  ports: PortRecord
+}
+
+export type PortRecord = Record<string, PortEntry>
 
 export type PortEntry = {
   name: string
@@ -16,17 +36,7 @@ export type PortEntry = {
   connection?: Connection
 }
 
-export type PortRecord = Record<string, PortEntry>
-
-export type NodeEntry = {
-  id: string
-  name: string
-  url: URL
-  ports: PortRecord
-}
-
-export type Net = {
-  edges: Array<Edge>
-  activeEdges: Array<Edge>
-  nodeEntries: Map<string, NodeEntry>
+export type Connection = {
+  edge: Edge
+  port: Port
 }
