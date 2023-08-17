f12232e2f768f25e3af9546ac9c7b530a346304f
Xie Yuheng <xyheme@gmail.com>
2023-08-17 12:21:20 +0800

    extract formatEdge from formatNet

--- src/lang/net/formatNet.ts
+++ src/lang/net/formatNet.ts
@@ -1,24 +1,19 @@
+import { Edge } from "../edge"
 import { formatNode } from "../node/formatNode"
 import { Net } from "./Net"
 import { allEdges } from "./allEdges"
 
 export function formatNet(net: Net): string {
-  const lines: Array<string> = []
+  return allEdges(net).map(formatEdge).join("\n")
+}
 
-  for (const edge of allEdges(net)) {
-    const first = formatNode(edge.first.node)
-    const second = formatNode(edge.second.node)
+function formatEdge(edge: Edge): string {
+  const first = formatNode(edge.first.node)
+  const second = formatNode(edge.second.node)
 
-    if (edge.first.isPrincipal && edge.second.isPrincipal) {
-      lines.push(
-        `(${first})-${edge.first.name}!${edge.second.name}-(${second})`,
-      )
-    } else {
-      lines.push(
-        `(${first})-${edge.first.name} ${edge.second.name}-(${second})`,
-      )
-    }
+  if (edge.first.isPrincipal && edge.second.isPrincipal) {
+    return `(${first})-${edge.first.name}!${edge.second.name}-(${second})`
+  } else {
+    return `(${first})-${edge.first.name} ${edge.second.name}-(${second})`
   }
-
-  return lines.join("\n")
 }
