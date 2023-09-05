cc3d2e042c40fb2e77bb22112dd1008c66da3f3d
Xie Yuheng <xyheme@gmail.com>
2023-09-05 14:44:13 +0800

    up

--- src/lang/mod/findNodeRuleEntries.ts
+++ src/lang/mod/findNodeRuleEntries.ts
@@ -2,7 +2,6 @@ import { NodeWithoutId } from "../node"
 import { nodeKeyWithoutId } from "../node/nodeKeyWithoutId"
 import { Mod } from "./Mod"
 import { RuleEntry } from "./RuleEntry"
-import { hasNodeDefinition } from "./hasNodeDefinition"
 
 export function findNodeRuleEntries(
   mod: Mod,
@@ -12,12 +11,8 @@ export function findNodeRuleEntries(
   const entries: Array<RuleEntry> = []
   for (const [key, entry] of mod.ruleEntries) {
     const [firstKey, secondKey] = key.split(" ")
-    if (
-      (firstKey === nodeKey && hasNodeDefinition(mod, entry.second)) ||
-      (firstKey === nodeKey && hasNodeDefinition(mod, entry.first)) ||
-      (secondKey === nodeKey && hasNodeDefinition(mod, entry.first)) ||
-      (secondKey === nodeKey && hasNodeDefinition(mod, entry.second))
-    ) {
+
+    if (firstKey === nodeKey || secondKey === nodeKey) {
       if (!entries.find(({ name }) => name === entry.name)) {
         entries.push(entry)
       }
